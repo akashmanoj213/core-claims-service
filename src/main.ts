5927350bea7b891951a48c3f679abc839e7ca985
Akash Manoj <akashmanoj213@gmail.com>
2023-10-18 12:37:36 +0530

    Added logger

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as lb from '@google-cloud/logging-bunyan';
 
 async function bootstrap() {
+  const { mw } = await lb.express.middleware();
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -22,6 +24,8 @@ async function bootstrap() {
   );
 
   app.enableCors();
+  app.use(mw);
+
   await app.listen(parseInt(process.env.PORT) || 8080);
 }
 bootstrap();
