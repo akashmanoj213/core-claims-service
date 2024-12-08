be9dbe08e0707869cf8890ae8b9644cfcd3cb6af
Akash Manoj <akashmanoj213@gmail.com>
2024-12-08 17:33:11 +0530

    added .env

--- apps/grpc-project/src/grpc-project.module.ts
+++ apps/grpc-project/src/grpc-project.module.ts
@@ -3,9 +3,14 @@ import { GrpcProjectController } from './grpc-project.controller';
 import { GrpcProjectService } from './grpc-project.service';
 import { OwnerModule } from './owner/owner.module';
 import { CommonServicesModule } from '@app/common-services';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [OwnerModule, CommonServicesModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: 'apps/grpc-project/.env' }),
+    OwnerModule,
+    CommonServicesModule,
+  ],
   controllers: [GrpcProjectController],
   providers: [GrpcProjectService],
 })
