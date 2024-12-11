aff1ae659120c1318951581aa93b353847825c93
Akash Manoj <akashmanoj213@gmail.com>
2024-12-11 14:58:59 +0530

    trying with in-memory tracer and allInsutumentation

--- apps/core-claims-service/src/open-telemetry-sdk.ts
+++ apps/core-claims-service/src/open-telemetry-sdk.ts
@@ -28,18 +28,14 @@ export function initializeOtelSdk(serviceName: string) {
       [ATTR_SERVICE_NAME]: serviceName,
       [ATTR_SERVICE_VERSION]: '1.0',
     }),
-    // traceExporter: new tracing.InMemorySpanExporter(),
-    spanProcessor: new tracing.BatchSpanProcessor(
-      new tracing.ConsoleSpanExporter(),
-    ),
+    traceExporter: new tracing.InMemorySpanExporter(),
+    // spanProcessor: new tracing.BatchSpanProcessor(
+    //   new tracing.ConsoleSpanExporter(),
+    // ),
     // logRecordProcessors: [
     //   new BatchLogRecordProcessor(new ConsoleLogRecordExporter()),
     // ],
-    instrumentations: [
-      new HttpInstrumentation(),
-      new ExpressInstrumentation(),
-      new WinstonInstrumentation(),
-    ],
+    instrumentations: [getNodeAutoInstrumentations()],
   });
 
   try {
